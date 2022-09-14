fee7b770e7f8e52bcacb949e8e0b7e729aeaf1f8
salmanquixel <salman@quixel.se>
2022-09-14 17:08:55 +0500

    bug fix

--- pages/feature/serverSideRendering.tsx
+++ pages/feature/serverSideRendering.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import HomeButton from "../../components/HomeButton";
+import NavigationButton from "../../components/NavigationButton";
 import styles from "../../styles/Home.module.css";
 
 const StaticGeneration: NextPage = () => {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <HomeButton />
+        <NavigationButton Path="/" ButtonText={"Home"} corner={"TL"} />
         <h1 className={styles.title}>Server Side Generation</h1>
         <h3> Under Construction</h3>
         <h6> Come back, another day</h6>
